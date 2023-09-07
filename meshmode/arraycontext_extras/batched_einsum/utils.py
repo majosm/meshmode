46d09071ffc0dbd918eade22e644c81acbc15674
Matthew Smith <mjsmith6@illinois.edu>
2023-09-07 13:47:00 -0500

    add Kaushik's fix for loops of different lengths being erroneously fused

--- meshmode/arraycontext_extras/batched_einsum/utils.py
+++ meshmode/arraycontext_extras/batched_einsum/utils.py
@@ -26,6 +26,7 @@ THE SOFTWARE.
 """
 
 from dataclasses import dataclass
+import numpy as np
 from typing import (
     TYPE_CHECKING, Any, Callable, Dict, FrozenSet, List, Mapping, Set, Tuple, Type)
 
@@ -187,10 +188,14 @@ def apply_kennedy_fusion_with_batched_einsum_extension(
 
             if insn.reduction_inames():
                 einsum, _ = fnsm.get_a_matched_einsum(
-                    t_unit, insn_match=lp_match.Id(insn.id))
+                    t_unit, insn_match=lp_match.Id(insn.id),
+                    # only consider inames with same length for fusion
+                    # => do not parametrize inames with very long loop-counts.
+                    long_dim_length=np.inf)
                 einsum = fnsm.canonicalize_einsum(einsum)
                 subst_map = fnsm.match_t_unit_to_einsum(
-                    t_unit, einsum, insn_match=lp_match.Id(insn.id))
+                    t_unit, einsum, insn_match=lp_match.Id(insn.id),
+                    long_dim_length=np.inf)
             else:
                 # we treat any non-reduction einsum as a copy-einsum
                 assignee = insn.assignee
